243092cae1379d86f10f5dc14b4db8d9759cc0fc
damdam322 <adamguohuangdi@gmail.com>
2025-06-22 19:48:57 +0200

    added verbose setting

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+#check for input
+if len(sys.argv) < 2:
+        sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+messages = [types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),]
+response = client.models.generate_content(model= "gemini-2.0-flash-001", contents=messages)
 
-response = client.models.generate_content(model= "gemini-2.0-flash-001", contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+def print_response():
+    print(response.text)
+    if len(sys.argv) == 3:
+         if sys.argv[2] == "--verbose":
+            print(f"User prompt: {sys.argv[1]}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    return
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+def main():
+    print_response()
+
+if __name__ == '__main__':
+  main()
+  sys.exit(0)
