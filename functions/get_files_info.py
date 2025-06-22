8526a443f468a5da8d42f1d87e94301615ec671c
damdam322 <adamguohuangdi@gmail.com>
2025-06-22 21:32:37 +0200

    get_files_info works

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,22 +1,20 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    if not os.path.isdir(directory):
+    dic = os.path.join(working_directory, directory)
+    if not os.path.isdir(dic):
         return f'Error: "{directory}" is not a directory'
-    
-    directory_abs = os.path.abspath(directory)
-    working_directory_abs = os.path.abspath(working_directory)
 
-    if not directory_abs.startswith(working_directory_abs):
+    if not os.path.abspath(dic).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    list_of_items = os.listdir(path=directory)
+    list_of_items = os.listdir(path=dic)
     output_string = ""
 
     for item in list_of_items:
         name = item
-        size = str(os.path.getsize(os.path.join(directory, item)))
-        type = not os.path.isfile(os.path.join(directory, item))
+        size = str(os.path.getsize(os.path.join(dic, item)))
+        type = not os.path.isfile(os.path.join(dic, item))
         output_string += f"- {name}: file_size={size} bytes, is_dir={type}\n"
 
     return output_string
