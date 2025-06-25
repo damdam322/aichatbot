803a75b404f1b8a7780e496ef3be8514d7e4b091
damdam <damdam@fedora.home>
2025-06-26 00:25:27 +0200

    added write file

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,6 +3,29 @@ import os
 def write_file(working_directory, file_path, content):
     dic = os.path.join(working_directory, file_path)
     if not os.path.abspath(dic).startswith(os.path.abspath(working_directory)):
-        return f"Cannot write to {file_path} as it is outside the permitted working directory"
-    if not os.path.exists(dic):
-        if os.makedirs(dic)
+        return f"Error: Cannot write to {file_path} as it is outside the permitted working directory"
+    
+    #validate if path is file path
+    is_end_file = False
+    try:
+        for i in reversed(dic):
+            if i == "/" and is_end_file == False:
+                return f"Cannot write to {file_path} file path is not file path example invalid: path/path valid: path/file.txt"
+            if i == "/" and is_end_file:
+                break
+            if i == ".":
+                is_end_file = True
+
+        if os.path.exists(dic):
+            with open(dic, "w") as f:
+                f.write(content)
+        else:
+            os.makedirs(os.path.dirname(dic), exist_ok=True)
+            f = open(dic, "w")
+            f.write(content)
+    except Exception as e:
+        return f"Error: {e}"
+    
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    
+    
